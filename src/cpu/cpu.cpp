1d321dfdb1a1b6a1b85155621cfa4fe3a788ba06
Dru Still <eugene_still@brown.edu>
2025-04-03 15:51:40 -0400

    Register Struct

---
+++ src/cpu/cpu.cpp
@@ -0,0 +1,8 @@
+struct Registers
+{
+  union { struct {uint8_t a, f; } uint16_t af; };
+  union { struct {uint8_t b, c; } uint16_t bc; };
+  union { struct {uint8_t d, e; } uint16_t de; };
+  union { struct {uint8_t h, l; } uint16_t hl; };
+  uint16_t sp, pc;
+}
