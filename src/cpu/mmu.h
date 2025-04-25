963be4531b5a32723e0d273ce6de11e66661bc7d
Dru Still <eugene_still@brown.edu>
2025-04-25 14:35:29 -0400

    mv defines.h definitions.h

--- src/cpu/mmu.h
+++ src/cpu/mmu.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "../defines.h"
+#include "../definitions.h"
 
 #include <iostream>
 #include <cstdint>
--- src/cpu/register.h
+++ src/cpu/register.h
@@ -1,4 +1,4 @@
-#include "../defines.h"
+#include "../definitions.h"
 
 class ByteRegister
 {
