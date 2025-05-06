638b378c693abc2d536dcef006cd6ba7be246667
Dru Still <eugene_still@brown.edu>
2025-05-06 15:59:05 -0400

    MMU using io data range

--- src/cpu/mmu.cpp
+++ src/cpu/mmu.cpp
@@ -2,29 +2,32 @@
 
 byte MMU::read(word addr)
 {
-  switch (addr) {
-    default:
-      return data[addr];
+  if (addr >= 0xFF00 & addr <= 0xFF7F) {
+    return io[addr - 0xFF00];
   }
+
+  return data[addr];
 }
 
 void MMU::write(word addr, byte value)
 {
-  data[addr] = value;
+  if (addr >= 0xFF00 & addr <= 0xFF7F) {
+    io[addr - 0xFF00] = value;
 
-  switch (addr) {
-    case 0xFF01:
-      serial_data = value;
-      break;
+    switch (addr) {
+      case 0xFF01:
+        serial_data = value;
+        break;
 
-    case 0xFF02:
-      if (value & 0x80) {
-        std::cout << static_cast<char>(serial_data);
-        std::flush(std::cout);
-      }
-      break;
+      case 0xFF02:
+        if (value & 0x80) {
+          std::cout << static_cast<char>(serial_data);
+          std::flush(std::cout);
+        }
+        break;
+    }
 
-    default:
-      break;
+  } else {
+    data[addr] = value;
   }
 }
