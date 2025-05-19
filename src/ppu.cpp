bfb865c24fa30fccd7f408fbc1fc396f2825c4a7
Dru Still <eugene_still@brown.edu>
2025-05-19 17:01:02 -0400

    draw sprite function

--- src/ppu.cpp
+++ src/ppu.cpp
@@ -1,8 +1,11 @@
 #include "ppu.h"
+#include "bitwise.h"
 
 PPU::PPU(MMU* mmu) :
   mmu(mmu),
-  cycles(0)
+  cycles(0),
+  sprite_screen(WIDTH, HEIGHT),
+  background_screen(WIDTH, HEIGHT)
 { }
 
 void PPU::tick(int cycles)
@@ -101,14 +104,71 @@ void PPU::vram()
   {
     update_lcd_mode(VideoMode::HBlank);
     this->cycles -= 173;
-
-    // TODO : ????? Where do we handle the Mode3 penalties OBJ penalty?
   }
 };
+
+void PPU::write_sprites()
+{
+  for (int sprite_num = 0; sprite_num < 40; sprite_num++)
+  {
+    draw_sprite(sprite_num);
+  }
+}
+void PPU::draw_sprite(const unsigned int sprite_num)
+{
+  word oam_address = 0xFE00 + (sprite_num * 4);
+  byte sprite_y = mmu->read(oam_address);
+  byte sprite_x = mmu->read(oam_address + 1);
+
+  if (sprite_y == 0 || sprite_y >= 160) return;
+  if (sprite_x == 0 || sprite_x >= 168) return;
+
+  unsigned int sprite_size = bitwise::test_bit(mmu->read(0xFF40), 5) ? 2 : 1;
+
+  byte sprite_index = mmu->read(oam_address + 2);
+  byte sprite_attributes = mmu->read(oam_address + 3);
+
+  bool use_obp1 = bitwise::test_bit(sprite_attributes, 4);
+  bool flip_x = bitwise::test_bit(sprite_attributes, 5);
+  bool flip_y = bitwise::test_bit(sprite_attributes, 6);
+  bool behind_background = bitwise::test_bit(sprite_attributes, 7);
+
+  word sprite_tile_block = 0x8000; // block 0
+  byte pixel_offset = sprite_index * 16; // 8 2-byte lines in a tile
+  byte pixel_index = sprite_tile_block + pixel_offset;
+
+  int viewport_y = sprite_y - 16;
+  int viewport_x = sprite_x - 8;
+
+  for (int y = 0; y < 8 * sprite_size; y++)
+  {
+    for (int x = 0; x < 8; x++)
+    {
+      Color tile_color = Color::White; // TODO
+
+      int screen_y = viewport_y + y;
+      int screen_x = viewport_x + x;
+
+      Color current_color = sprite_screen.get_pixel(screen_x, screen_y);
+
+      if (tile_color == Color::Transparent) continue;
+      if (!pixel_on_screen(screen_x, screen_y)) continue;
+      if (behind_background && current_color != Color::White) continue;
+
+      Color screen_color = Color::DarkGray; // TODO
+      sprite_screen.set_pixel(screen_x, screen_y, screen_color);
+    }
+  }
+}
 
 void PPU::update_lcd_mode(VideoMode m)
 {
   mmu->PPUMode = m;
   uint8_t masked = mmu->stat.get() & 0xFC;
   mmu->write(0xFF41, masked | (uint8_t)m);
+}
+
+bool PPU::pixel_on_screen(unsigned int x, unsigned int y)
+{
+  return x < WIDTH && y < 144;
 }
--- src/ppu.h
+++ src/ppu.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "mmu.h"
+#include "shiftregister.h"
 
 const int WIDTH = 160;
 const int HEIGHT = 144;
@@ -12,20 +13,21 @@ class PPU
 
     void tick(int cycles);
 
-    std::vector<Color> sprite_data;
-    std::vector<Color> background_data;
+    ShiftRegister sprite_screen;
+    ShiftRegister background_screen;
 
   private:
     MMU* mmu;
     int cycles; // AKA dots
-
-    std::vector<Color> sprite_buffer;
-    std::vector<Color> background_buffer;
 
     void hblank();
     void vblank();
     void oam();
     void vram();
 
+    void write_sprites();
+    void draw_sprite(const unsigned int sprite_index);
+
     void update_lcd_mode(VideoMode m);
+    bool pixel_on_screen(unsigned int x, unsigned int y);
 };
